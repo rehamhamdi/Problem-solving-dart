c6eb47ebd992622dec94d50d0d1dba952144bb44
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2024-07-10 17:16:32 +0300

    Delete Task_2/CreateANewString.dart

--- Task_2/CreateANewString.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  String? S = stdin.readLineSync();
-  String? T = stdin.readLineSync();
-
-  print('${S?.split('').length}  ${T?.split('').length}');
-  print('$S $T');
-}
