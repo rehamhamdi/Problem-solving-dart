1ebf852d48779f240729506e1601229e235ed9d9
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2024-07-10 16:03:29 +0300

    Delete Task 2 directory

--- Task 2/CreateANewString.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  var stdin;
-  String ? S=stdin.readLineSync();
-      String ? T=stdin.readLineSync();
-      
-      print('${S?.split('').length}  ${T?.split('').length}');
-      print('$S $T');
-
-}
